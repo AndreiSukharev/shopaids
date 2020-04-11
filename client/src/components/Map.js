1011b55dd00ecb2b46b041a98c28cd9743353441
jbarlukov <maestroz89@gmail.com>
2020-04-11 15:00:08 +0800

    Added markers. Improved map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -6,15 +6,15 @@ export const ShopsMap = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `100%` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
   withGoogleMap
 )((props) =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={15}
+    defaultCenter={{ lat: 51.522114, lng: -0.157575 }}
   >
     {props.markers && props.markers.map(marker => (
       <Marker position={{ lat: marker.lat, lng: marker.lng }} />
