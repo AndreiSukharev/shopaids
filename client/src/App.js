5d62cb57e1a56f12bc082a815e8124f2b5f40c06
jbarlukov <maestroz89@gmail.com>
2020-04-11 14:28:03 +0800

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Route, Router } from 'react-router-dom';
 // import './App.css';
 import Header from "./components/Header";
+import MainPage from "./pages/main"
+import { createBrowserHistory } from 'history';
+const baseUrl = document.getElementsByTagName('base')[ 0 ].getAttribute('href');
+const history = createBrowserHistory({ basename: baseUrl });
 
 function App() {
     return (
-
         <div className="App">
-            <Header></Header>
+          <div className="page-content">
+            <Router history={history}>
+              <Route exact path='/' component={MainPage}/>
+              <Route exact path='/shopping-list' component={MainPage}/>
+            </Router>
+          </div>
+          <Header/>
         </div>
     );
 }
