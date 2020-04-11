8a4e5dd44ffe25d6182a77f9e87776d8875ee59d
Andrei Sukharev <mr.andrey.sd@gmail.com>
2020-04-11 11:41:01 +0300

    Updates

---
+++ precache-manifest.40d13ac98876806b03411dc910e556f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a3c7f21f4bcbbd253796c92bec0ccf",
+    "url": "/AndreiSukharev/shopaids/index.html"
+  },
+  {
+    "revision": "269e485e2819cf4425d8",
+    "url": "/AndreiSukharev/shopaids/static/css/main.72913935.chunk.css"
+  },
+  {
+    "revision": "c0a25b61893c7bf70021",
+    "url": "/AndreiSukharev/shopaids/static/js/2.92ff403d.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/AndreiSukharev/shopaids/static/js/2.92ff403d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269e485e2819cf4425d8",
+    "url": "/AndreiSukharev/shopaids/static/js/main.13b0f653.chunk.js"
+  },
+  {
+    "revision": "fd3712be16a40718edef",
+    "url": "/AndreiSukharev/shopaids/static/js/runtime-main.61606874.js"
+  }
+]);
