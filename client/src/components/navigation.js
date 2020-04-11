d8bb71ac9f4c1329457713afe20c9ad971dd0df0
Andrei Sukharev <mr.andrey.sd@gmail.com>
2020-04-11 08:04:38 +0300

    added pics nav bottom

--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
+import HomeIcon from '@material-ui/icons/Home';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PersonIcon from '@material-ui/icons/Person';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,17 +14,32 @@ import {
   Link
 } from "react-router-dom";
 
-export default class Header extends Component{
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
+export default class Header extends Component {
   render() {
+    // return (
+    // const classes = useStyles();
+    // const [value, setValue] = React.useState(0);
 
     return (
       <BottomNavigation
+        // value={value}
+        // onChange={(event, newValue) => {
+        //     setValue(newValue);
+        // }}
         showLabels
+        // className={classes.root}
       >
         <Router>
-          <BottomNavigationAction component={Link} label="Recents"  to="/Recents" icon={<RestoreIcon />} />
-          <BottomNavigationAction label="Favorites" component={Link}  to="/Favorites" icon={<FavoriteIcon />} />
-          <BottomNavigationAction component={Link} label="Nearby" to="/Nearby" icon={<LocationOnIcon />} />
+          <BottomNavigationAction component={Link} label="Home" to="/home" icon={<HomeIcon/>}/>
+          <BottomNavigationAction label="Stores" component={Link} to="/stores" icon={<FavoriteIcon/>}/>
+          <BottomNavigationAction component={Link} label="Lists" to="/lists" icon={<ListAltIcon/>}/>
+          <BottomNavigationAction component={Link} label="Profile" to="/profile" icon={<PersonIcon/>}/>
         </Router>
       </BottomNavigation>
     )
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Loadable from 'react-loadable'
 import LoadingComponent from 'rmw-shell/lib/components/LoadingComponent'
-//import App from './App'
+import App from './App'
 
 const Loading = () => <LoadingComponent />
 export const AppAsync = Loadable({
