2556d70172bc8fcc6ec6f36d62638ea040509642
jbarlukov <maestroz89@gmail.com>
2020-04-11 16:05:34 +0800

    Added theme base

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,32 @@ import React from 'react'
 import { Route, Router } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header'
+import { ThemeProvider } from '@material-ui/styles'
 import MainPage from './pages/main'
 import ShoppingList from './pages/ShoppingList'
 import { createBrowserHistory } from 'history'
 import ShopsMapPage from './pages/ShopsMapPage'
 import Home from './pages/home'
+import theme from './theme'
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')
 const history = createBrowserHistory({ basename: baseUrl })
 
 function App() {
   return (
-    <Router history={history}>
-      <div className="App">
-        <div className="page-content">
-          <Route exact path='/' component={MainPage}/>
-          <Route exact path='/home' component={Home}/>
-          <Route exact path='/stores' component={ShopsMapPage}/>
-          <Route exact path='/list' component={ShoppingList}/>
+    <ThemeProvider theme={theme}>
+      <Router history={history}>
+        <div className="App">
+          <div className="page-content">
+            <Route exact path='/' component={MainPage}/>
+            <Route exact path='/home' component={Home}/>
+            <Route exact path='/stores' component={ShopsMapPage}/>
+            <Route exact path='/list' component={ShoppingList}/>
+          </div>
+          <Header/>
         </div>
-        <Header/>
-      </div>
-    </Router>
+      </Router>
+    </ThemeProvider>
   )
 }
 
---
+++ client/src/theme.js
@@ -0,0 +1,20 @@
+import { createMuiTheme } from '@material-ui/core'
+
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#E25F3E',
+      dark: '#B99471',
+      light: '#F0E4D7'
+    },
+    secondary: {
+      main: '#5156B5',
+      dark: '#353BA8',
+      light: '#777CCB'
+    },
+    background: {
+      paper: 'rgba(250, 248, 248, 0.92)'
+    }
+  }
+})
