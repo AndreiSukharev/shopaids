259f4d977c7586e106a1c02d80b2628d560827a2
Andrei Sukharev <mr.andrey.sd@gmail.com>
2020-04-11 09:34:33 +0300

    items

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import './App.css';
 import Header from "./components/Header";
+import Items from "./pages/Items";
 
 function App() {
     return (
 
         <div className="App">
+          <Items></Items>
             <Header></Header>
         </div>
     );
---
+++ client/src/pages/Items.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function NativeSelects() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    age: '',
+    name: 'hai',
+  });
+
+  const handleChange = (event) => {
+    const item = event.target.item;
+    setState({
+      ...state,
+      [item]: event.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="item1">Item</InputLabel>
+        <Select
+          native
+          value={state.item}
+          onChange={handleChange}
+          inputProps={{
+            name: 'item',
+            id: 'item1',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={"Milk"}>Milk</option>
+          <option value={"Bread"}>Bread</option>
+          <option value={"Toilet Paper"}>Toilet Paper</option>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="item1">Item</InputLabel>
+        <Select
+          native
+          value={state.item}
+          onChange={handleChange}
+          inputProps={{
+            name: 'item',
+            id: 'item2',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={"Milk"}>Milk</option>
+          <option value={"Bread"}>Bread</option>
+          <option value={"Toilet Paper"}>Toilet Paper</option>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="item1">Item</InputLabel>
+        <Select
+          native
+          value={state.item}
+          onChange={handleChange}
+          inputProps={{
+            name: 'item',
+            id: 'item3',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={"Milk"}>Milk</option>
+          <option value={"Bread"}>Bread</option>
+          <option value={"Toilet Paper"}>Toilet Paper</option>
+        </Select>
+      </FormControl>
+
+    </div>
+  );
+}
