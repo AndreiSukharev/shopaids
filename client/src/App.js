64c80781f21c7fec4e242feb7d87735c89dd8265
Andrei Sukharev <mr.andrey.sd@gmail.com>
2020-04-18 13:34:55 +0300

    added dynamic products

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Route, Router } from 'react-router-dom'
-import './App.css';
+import './App.css'
 import Header from './components/Header'
 import { ThemeProvider } from '@material-ui/styles'
 import Login from './pages/Login'
@@ -11,7 +11,8 @@ import ShopsMapPage from './pages/ShopsMapPage'
 import Home from './pages/home'
 import theme from './theme'
 import DirectionsService from './services/DirectionsService'
-import { toast } from 'react-toastify';
+import Context from './context'
+import { toast } from 'react-toastify'
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')
 export const history = createBrowserHistory({ basename: baseUrl })
@@ -20,25 +21,27 @@ export const services = {
 }
 
 toast.configure({
-  position: "bottom-right",
+  position: 'bottom-right',
   autoClose: 2000,
   hideProgressBar: true,
   closeOnClick: true,
-});
+})
 
 function App() {
-
+  const [products, updateProducts] = useState([{name: "Milk", qty: 12}])
 
   return (
     <ThemeProvider theme={theme}>
       <Router history={history}>
         <div className="App">
           <div className="page-content">
-            <Route exact path='/shopaid' component={Login}/>
-            <Route exact path='/shopaid/home' component={Home}/>
-            <Route exact path='/shopaid/stores' component={ShopsMapPage}/>
-            <Route exact path='/shopaid/list' component={ShoppingList}/>
-            <Route exact path='/shopaid/profile' component={Profile}/>
+            <Context.Provider value={products}>
+              <Route exact path='/shopaid' component={Login}/>
+              <Route exact path='/shopaid/home' component={Home}/>
+              <Route exact path='/shopaid/stores' component={ShopsMapPage}/>
+              <Route exact path='/shopaid/list' component={ShoppingList}/>
+              <Route exact path='/shopaid/profile' component={Profile}/>
+            </Context.Provider>
           </div>
           <Header/>
         </div>
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,49 +1,72 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import FormGroup from '@material-ui/core/FormGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Paper from '@material-ui/core/Paper'
-import Checkbox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 import TextField from '@material-ui/core/TextField'
+import AddIcon from '@material-ui/icons/Add';
 import { Typography } from '@material-ui/core'
 import useStyles from '../classes'
 
-export default function CheckboxesGroup() {
+import Context from "../context";
+
+export default function Home() {
   const classes = useStyles()
-  const [state, setState] = React.useState({
-    milk: false,
-    bread: false,
-    paper: false,
-  })
+  const products = useContext(Context)
+  const [newProduct, updateNewProduct] = useState({name:"", qty: 1})
 
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked })
+  // function updateQty(val) {
+  //   updateNewProduct({name, qty: val})
+  // }
+  function addProduct() {
+    // const prod = {name: newProduct, qty: 1}
+    updateNewProduct({name:"", qty: 1})
+    products.push(newProduct)
+    // updateProducts(updateProducts)
   }
-
   return (
     <div className={ classes.verticalFlex}>
       <h2><Typography color="" variant="h5">What items do you want to buy?</Typography></h2>
-
       <Paper className={[classes.flexItemMain]}>
         <div>
-          {['Eggs', 'Pasta', 'Toilet rolls'].map(product => (
+          {products.map((product, index) => (
             <FormGroup className={classes.formGroup}>
               <div className={classes.flex}>
-                <FormControlLabel
-                  className={classes.flexItemMain}
-                  control={<Checkbox color="primary" checked={state[product]} onChange={handleChange} name={product}/>}
-                  label={product}
+                <TextField
+                  label="Product"
+                  defaultValue={product.name}
+                  InputProps={{
+                    readOnly: true,
+                  }}
                 />
-                <TextField id={`${product}-qty`} label="Qty" size="small"/>
+                &nbsp;
+                <TextField id={index} label="Qty" value={product.qty}/>
               </div>
             </FormGroup>
           ))}
+          <form style={{minWidth: "20em"}} noValidate autoComplete="off">
+            <TextField
+              id="addProduct"
+              label="Type items like Milk"
+              onChange={event => updateNewProduct({...newProduct, name: event.target.value })}
+              value={newProduct.name}
+            />
+            &nbsp;
+            <TextField
+              label="Qty"
+              onChange={event => updateNewProduct({...newProduct, qty: event.target.value})}
+              value={newProduct.qty}
+            />
+            <IconButton onClick={addProduct}>
+              <AddIcon/>
+            </IconButton>
+          </form>
         </div>
       </Paper>
 
       <div className={classes.m}>
-        <Button variant="contained" color="primary" component={Link} to="/shopaids/stores" fullWidth>
+        <Button variant="contained" color="primary" component={Link} to="/shopaid/stores" fullWidth>
           Continue
         </Button>
       </div>
