b9aeda650cd19a7662c9525ecb9ca2f5f1173a1d
Andrei Sukharev <mr.andrey.sd@gmail.com>
2020-04-18 17:06:48 +0300

    Updates

---
+++ precache-manifest.21c9894ed44797f5e88e18e817809607.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0863901455867c5df48ace0983ef6097",
+    "url": "/shopaids/index.html"
+  },
+  {
+    "revision": "c8315623fbd7f9946dc1",
+    "url": "/shopaids/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "ec3d7dda84b84b8a9a1c",
+    "url": "/shopaids/static/css/main.bed88b16.chunk.css"
+  },
+  {
+    "revision": "c8315623fbd7f9946dc1",
+    "url": "/shopaids/static/js/2.fafed836.chunk.js"
+  },
+  {
+    "revision": "2badc1b209c5a040c707dcd720b10f8a",
+    "url": "/shopaids/static/js/2.fafed836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3d7dda84b84b8a9a1c",
+    "url": "/shopaids/static/js/main.0e118cd7.chunk.js"
+  },
+  {
+    "revision": "0cf04433d9ff48183791",
+    "url": "/shopaids/static/js/runtime-main.864ef602.js"
+  },
+  {
+    "revision": "58b74084f931d3fe75ea7f72b4aba884",
+    "url": "/shopaids/static/media/shopaid-logo.58b74084.png"
+  }
+]);
